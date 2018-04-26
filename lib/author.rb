19757d6d6f4582798caee34343e64ca4c23f02f4
Harley Rayner <harleymteden@gmail.com>
2018-04-26 17:30:39 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,26 +2,29 @@ class Author
 
   attr_accessor :name
 
+  @@post_count = 0
+
   def initialize(name)
     @name = name
     @posts = []
   end
-  #
-  # def author
-  #   self
-  # end
 
   def add_post(post)
     post.author = self
     @posts << post
+    @@post_count += 1
   end
 
   def add_post_by_title(title)
-      post = Post.new(title)
-      post.author = self
-      @posts << post
-      # @@post_count += 1
+    post = Post.new(title)
+    post.author = self
+    @posts << post
+    @@post_count += 1
     end
+
+  def self.post_count
+
+  end
 
   # def author
   #   @name
