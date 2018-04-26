551ed775943127f8e13eaab4ca95b3a837ae32a0
Harley Rayner <harleymteden@gmail.com>
2018-04-26 17:24:37 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,13 +1,15 @@
 class Post
   attr_accessor :author, :title
 
-  def initialize(author)
-    author = @author
+  def initialize(titlle)
+    @title = title
   end
 
-  def title
-    @title
-  end 
+  # def title
+  #   @title
+  # end
 
-
+  # def author
+  #
+  # end
 end
