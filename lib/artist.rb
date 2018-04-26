5fefbf8a3c28d2a52de4171383b74b74ba2fd23e
Harley Rayner <harleymteden@gmail.com>
2018-04-26 16:21:45 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,21 +5,24 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
+    @@song_count = 0
   end
 
   def add_song(song)
    @songs << song
    song.artist = self
+   @@song_count += 1
   end
 
   def add_song_by_name(songname)
     song = Song.new(songname)
     @songs << song
     song.artist = self
+    @@song_count += 1
   end
 
   def song_count
-
+    @@song_count
   end
 
   def songs
