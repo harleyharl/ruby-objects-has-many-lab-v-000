256b2d888417d6d635b472fb4ac2ab28cd01c41c
Harley Rayner <harleymteden@gmail.com>
2018-04-26 16:23:37 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,14 +22,12 @@ class Artist
     @@song_count += 1
   end
 
-  def song_count
-    @@song_count
-  end
+  # def song_count
+  #   @@song_count
+  # end
 
   def songs
     @songs
   end
-
-
 
 end
